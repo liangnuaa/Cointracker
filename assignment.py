7bf6328c3b97f0d6462f059fa2db6491187593da
Liang Zhang <zhangliangnuaa@gmail.com>
2021-10-24 17:09:19 -0700

    add test to verify three tables

--- assignment.py
+++ assignment.py
@@ -229,6 +229,26 @@ def add_transactions(conn, transactions):
         create_transaction(conn, transaction)
 
 
+def get_all_records(conn):
+    """
+    Get all records from table
+    :param conn: the Connection object
+    :param table_name:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+
+    users = cur.fetchall()
+    cur.execute("SELECT * FROM addresses")
+
+    addresses = cur.fetchall()
+    cur.execute("SELECT * FROM transactions")
+
+    transactions = cur.fetchall()
+    return users, addresses, transactions
+
+
 def main():
     conn = create_connection('./test.db')
 
@@ -264,6 +284,15 @@ def main():
     new_transactions = retrieve_latest_transactions_details(new_transaction_hashes[95:], user_1_id)
     if new_transactions:
         add_transactions(conn, new_transactions)
+
+    users, addresses, transactions = get_all_records(conn)
+
+    expected_users = [(1, 'liangzhang', 'liang', 'zhang', 'zhangliang****@gmail.com', '206000001')]
+    expected_addresses = [(1, '3E8ociqZa9mZUSwGdSmAEMAoAxBK3FNDcd', '1', '2012839', '2021-10-24 17:06:28.854515')]
+    expected_transactions = [(1, '3b88828b1afee7314ac3dce908f9e061338f03051aa3d422bffad27d43a34b10', '3E8ociqZa9mZUSwGdSmAEMAoAxBK3FNDcd', 'bc1qzqh7y60gj525577m87l2svfv3rp0akcjy7k0zs', '1'), (2, 'ecaa28fca681c1b015f6261405815846059bac44ad574f71d7a9c128a19c91c4', '3FaL7eXpRSHBt8YeyVR7nfuRXydkv8ch21', '3E8ociqZa9mZUSwGdSmAEMAoAxBK3FNDcd', '1'), (3, '6ae8ced86239319f8b7eba2b4842589506265033d8325183cfe78becaa85a615', 'bc1qs7qpqjxy9ahpr3ddf9rhhqusjjd3vkc5fr7zh7', '3LnV7DjSaB5qZY1n7NkwK5HABEFNrznc9y', '1'), (4, '29f9f0930e93554577e79842acc335b83972fa6e5e93d2b62318b74eff582604', 'bc1q3jzpzukg63u0mq3ualdfmv5f6laq7ts737eets', 'bc1q6af60xnjzwukts6z4u9235jgjp4ced2zlwsan7', '1'), (5, '246aef6f4f8a8a9325b902180c47135df04a67cbaceb0065b0ede57654301c70', 'bc1qfmvpylmay5gm2mznhezp8kp2v9asah0utnuyuj', '3E8ociqZa9mZUSwGdSmAEMAoAxBK3FNDcd', '1')]
+    print(users == expected_users)
+    print(addresses == expected_addresses)
+    print(transactions == expected_transactions)
 
 
 if __name__ == '__main__':
